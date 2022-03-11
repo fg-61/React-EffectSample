aa70a37a43ba90e0683ddd45a2e065ca05d9b6b1
Fatih Gören <fatihgoren61@gmail.com>
2022-03-11 17:40:52 +0300

    useState-useEffect

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import EffectSample from './effectSample/EffectSample';
+import EffectSample2 from './effectSample/EffectSample2';
+
+
+function App() {
+
+  
+  return (
+    <>
+      <EffectSample></EffectSample>
+      <EffectSample2></EffectSample2>
+    </>
+  )
+}
+
+export default App;
